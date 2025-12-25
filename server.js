9fcab9d2035b5fd215ac846edaba23d22d56003e
aquavistapure <aquavistapure@gmail.com>
2025-12-25 19:50:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,30 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("DB Connected"))
-.catch(err => console.log(err));
-
-app.get("/", (req, res) => {
-  res.send("AquaVistaPure backend running");
-});
+// ROUTES
 const productRoutes = require("./routes/productRoutes");
 app.use("/api/products", productRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running on port 5000");
+// TEST ROUTE
+app.get("/", (req, res) => {
+  res.send("AquaVistaPure backend running");
+});
+
+// DB CONNECT
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
+
+// PORT (IMPORTANT FOR RENDER)
+const PORT = process.env.PORT || 5000;
+
+// START SERVER
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
